46db5f02e96b6a5b3360392eddb07334f31766c1
Hr Habib <mdhabibur.hr7@gmail.com>
2024-07-03 10:25:35 +0600

    rtk query explored

--- src/components/todo/TodoModal.tsx
+++ src/components/todo/TodoModal.tsx
@@ -11,32 +11,36 @@ import {
 } from "../ui/dialog";
 import { Input } from "../ui/input";
 import { Label } from "../ui/label";
-import { useAppDispatch } from "../../redux/hook";
-import { addTodo } from "../../redux/features/todoSlice";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuLabel,
-  DropdownMenuRadioGroup,
-  DropdownMenuRadioItem,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "../ui/dropdown-menu";
+// import { useAppDispatch } from "../../redux/hook";
+// import { addTodo } from "../../redux/features/todoSlice";
+import { useAddTodoMutation } from "../../redux/api/api";
+import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger } from "../ui/select";
+import { SelectValue } from "@radix-ui/react-select";
 
 export function TodoModal() {
   const [task, setTask] = useState("");
   const [description, setDescription] = useState("");
-  const dispatch = useAppDispatch();
   const [priority, setPriority] = useState("medium");
-  console.log(priority);
+  // console.log(priority);
 
+  //! for local state management
+  // const dispatch = useAppDispatch();
+
+  // for sever
+  const [addTodo, { data, isLoading, isError, isSuccess }] = useAddTodoMutation();
+  console.log({ data, isLoading, isSuccess, isError });
   const onSubmit = (e: FormEvent) => {
     e.preventDefault();
 
-    const randomString = Math.random().toString(36).substring(2, 7);
+    // const randomString = Math.random().toString(36).substring(2, 7);
 
-    const taskDetails = { title: task, description, id: randomString, priority };
-    dispatch(addTodo(taskDetails));
+    const taskDetails = { title: task, description, priority };
+
+    //! for local state management
+    // dispatch(addTodo(taskDetails));
+
+    // for server
+    addTodo(taskDetails);
   };
 
   return (
@@ -64,23 +68,20 @@ export function TodoModal() {
               <Input onBlur={(e) => setDescription(e.target.value)} id="description" className="col-span-3" />
             </div>
             <div className="grid grid-cols-4 items-center gap-4">
-              <Label htmlFor="priority" className="text-right">
-                Priority
-              </Label>
-              <DropdownMenu>
-                <DropdownMenuTrigger asChild>
-                  <Button variant="outline">{priority}</Button>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent className="w-56">
-                  <DropdownMenuLabel>Select the task priority</DropdownMenuLabel>
-                  <DropdownMenuSeparator />
-                  <DropdownMenuRadioGroup value={priority} onValueChange={setPriority}>
-                    <DropdownMenuRadioItem value="high">High</DropdownMenuRadioItem>
-                    <DropdownMenuRadioItem value="medium">Medium</DropdownMenuRadioItem>
-                    <DropdownMenuRadioItem value="low">Low</DropdownMenuRadioItem>
-                  </DropdownMenuRadioGroup>
-                </DropdownMenuContent>
-              </DropdownMenu>
+              <Label className="text-right">Priority</Label>
+              <Select onValueChange={(value) => setPriority(value)}>
+                <SelectTrigger className="w-full col-span-3">
+                  <SelectValue placeholder="Select your priority" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    <SelectLabel>Priority</SelectLabel>
+                    <SelectItem value="High">High</SelectItem>
+                    <SelectItem value="Medium">Medium</SelectItem>
+                    <SelectItem value="Low">Low</SelectItem>
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
             </div>
           </div>
 
